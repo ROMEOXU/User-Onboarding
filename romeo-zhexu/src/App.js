4bda43a7f50def00d1558d0ca2dccee7d9f3a6cb
Mac <xuromeo@gmail.com>
2020-03-21 19:00:03 -0700

    need more work

---
+++ romeo-zhexu/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import CreateForm from './form';
+function App() {
+  return (
+    <div className="App">
+    <h1>Advanced Form Management</h1>
+    <h2> User Onboarding</h2>
+    <h3>by Romeo xu</h3>
+    <CreateForm />
+    </div>
+  );
+}
+
+export default App;
